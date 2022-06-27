8c48a8f54cc897feb78ad495816f423a8639a531
ruffcoins <nzubeanthony4@gmail.com>
2022-06-27 18:55:04 +0100

    Error 404 Page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Error404 from './../assets/images/error404.png';
+import {useNavigate} from "react-router-dom";
 
 const ErrorPage
 = () => {
+	const navigate = useNavigate();
 	return (
-		<div>
-			Error 404
-		</div>
+		<>
+			<div className="pt-24 flex justify-around items-center w-full h-full object-cover">
+				<img src={Error404} alt="error404" />
+			</div>
+		</>
+
 	);
 };
 
