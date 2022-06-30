acba254d351a10984826bb0ddebaeac3a15d1ab2
ruffcoins <nzubeanthony4@gmail.com>
2022-06-30 18:01:00 +0100

    increase T&C font weight

--- src/components/Terms.jsx
+++ src/components/Terms.jsx
@@ -4,9 +4,9 @@ const Terms = () => {
 	return (
 		<div className="md:py-40 pb-20 pt-8">
 			<div className="md:mx-52 mx-8 text-gray-600 md:text-2xl text-lg leading-relaxed">
-				<p className="my-5">TERMS OF USE</p>
+				<p className="my-5 uppercase font-semibold">TERMS OF USE</p>
 
-				<p className="my-5 uppercase">1. Introduction</p>
+				<p className="my-5 uppercase font-semibold">1. Introduction</p>
 
 				<p className="my-5">This website is operated by IHS Medical Inc. (IHS). The terms “we”, “us”, and “our” refer to IHS.
 					The use of our website is subject to the following terms and conditions of use, as amended
@@ -19,7 +19,7 @@ const Terms = () => {
 					access our website, use any of our products or complete service requests on any of our
 					provided platforms.</p>
 
-				<p className="my-5 uppercase">2. Use of our Website</p>
+				<p className="my-5 uppercase font-semibold">2. Use of our Website</p>
 
 				<p className="my-5">You agree to use our website and its auxiliary product(s) for legitimate purposes and not for
 					any illegal or unauthorized purpose, including without limitation, in violation of any intellectual
@@ -34,7 +34,7 @@ const Terms = () => {
 					You agree to promptly update your account and information. You authorize us to collect and
 					use this information to contact you in accordance with our Privacy Policy.</p>
 
-				<p className="my-5 uppercase">3. General Conditions</p>
+				<p className="my-5 uppercase font-semibold">3. General Conditions</p>
 
 				<p className="my-5">We reserve the right to refuse service to anyone, at any time, for any reason. We reserve the
 					right to make any modifications to the website, including terminating, changing, suspending, or
@@ -45,7 +45,7 @@ const Terms = () => {
 					or discontinuance of our website or for any service, content, feature, or product offered
 					through our website.</p>
 
-				<p className="my-5 uppercase">4. Products or Services</p>
+				<p className="my-5 uppercase font-semibold">4. Products or Services</p>
 
 				<p className="my-5">All purchases and registration through our website are subject to product or service availability.
 					We may, in our sole discretion, limit items offered on our website or limit the sale of our
@@ -63,7 +63,7 @@ const Terms = () => {
 					We have made efforts to ensure the colour, description and design of items are displayed as
 					accurately as possible on our website.</p>
 
-				<p className="my-5 uppercase">5. Links to Third-Party Websites</p>
+				<p className="my-5 uppercase font-semibold">5. Links to Third-Party Websites</p>
 
 				<p className="my-5">Links from or to websites outside our website are meant for convenience only. We do not
 					review, endorse, approve, or control, and are not responsible for any sites linked from or to our
@@ -74,7 +74,7 @@ const Terms = () => {
 					associated with downloading the software. Use of any downloaded software is governed by the
 					terms of the license agreement, if any, which accompanies or is provided with the software.</p>
 
-				<p className="my-5 uppercase">6. Use Comments, Feedback, and Other Submissions</p>
+				<p className="my-5 uppercase font-semibold">6. Use Comments, Feedback, and Other Submissions</p>
 
 				<p className="my-5">You acknowledge that you are responsible for the information, profiles, opinions, messages,
 					comments, and any other content (collectively, the “Content”) that you post, distribute, or
@@ -98,12 +98,12 @@ const Terms = () => {
 					delete any Content that we deem objectionable. You consent to such removal and/or deletion
 					and waive any claim against us for the removal and/or deletion of your Content.</p>
 
-				<p className="my-5 uppercase">7. Your Personal Information</p>
+				<p className="my-5 uppercase font-semibold">7. Your Personal Information</p>
 
 				<p className="my-5">Please see our Privacy Policy to learn about how we collect, use, and share your personal
 					information.</p>
 
-				<p className="my-5 uppercase">8. Errors and Omissions</p>
+				<p className="my-5 uppercase font-semibold">8. Errors and Omissions</p>
 
 				<p className="my-5">Please note that our website may contain typographical errors or inaccuracies and may not be
 					complete or current. We reserve the right to correct any errors, inaccuracies, or omissions and
@@ -115,7 +115,7 @@ const Terms = () => {
 					We do not undertake to update, modify, or clarify information on our website, except as
 					required by law.</p>
 
-				<p className="my-5 uppercase">9. Consent and Understanding</p>
+				<p className="my-5 uppercase font-semibold">9. Consent and Understanding</p>
 
 				<p className="my-5">By accessing and using this website, auxiliary product(s) and other IHS related services, you
 					understand and affirm that:
