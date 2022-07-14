d3fdd7f502b5d5efd81be4725a29dc479f0f7f77
ruffcoins <nzubeanthony4@gmail.com>
2022-07-14 19:02:29 +0100

    refactor sidebar

--- src/components/app/Layout.jsx
+++ src/components/app/Layout.jsx
@@ -22,7 +22,7 @@ const Layout = () => {
 
 	const activeLink = "flex items-center md:gap-5 gap-2 md:text-lg text-sm text-ihs-green bg-ihs-green-shade-200 border border-0 border-r-2 border-r-ihs-green md:pl-7 pl-3 py-2"
 	const normalLink = "flex items-center md:gap-5 gap-2 md:text-lg text-sm hover:bg-ihs-green-shade-100 border border-0 hover:border-r-2 hover:border-r-ihs-green md:pl-7 pl-3 py-2"
-	const sidebarStyle = "md:w-80 w-40 sticky left-0 top-0 border border-0 border-r border-slate-200 bg-cyan-50 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10"
+	const sidebarStyle = "drop-shadow-2xl md:w-80 w-40 sticky left-0 top-0 border border-0 border-r border-slate-200 bg-cyan-50 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10 z-10"
 
 	return (
 		<div>
@@ -30,13 +30,13 @@ const Layout = () => {
 				<div>
 					{/*Sidebar*/}
 					<div className={`${sidebarStyle} ${!sidebar ? "hidden" : !mobile ? handleMobile() : sidebarStyle}` }>
-						<div className="flex justify-center items-center px-4 border border-0 border-b border-slate-200">
+						<div className="flex justify-center items-center px-4">
 							<Link to="/dashboard" onClick={() => {
 							}} className="items-center my-4 flex">
 								<img src={Logo} alt="logo" className="w-40" />
 							</Link>
 						</div>
-						<div className="mt-10">
+						<div className="mt-5">
 							{links.map((item) => (
 								<NavLink to={`/${item.path}`} key={item.path} onClick={() => {}} className={({isActive}) => isActive ? activeLink : normalLink}>
 									{item.icon}
@@ -49,7 +49,7 @@ const Layout = () => {
 				</div>
 				<div className="grow">
 					{/*Navbar*/}
-					<nav className="flex justify-between h-20 border border-0 border-b border-slate-200 sticky top-0 z-10 bg-white">
+					<nav className="flex justify-between h-20 border border-0 border-b border-slate-200 sticky top-0 bg-white">
 						<button onClick={() => {toggleSidebar()}} className="md:invisible bg-transparent text-slate-500 text-xl border border-slate-300 hover:border-ihs-green-shade-100 m-4 p-2 block">
 							{ !sidebar ? <MenuIcon className="w-6"/> : <XIcon className="w-6"/> }
 						</button>
