52a836beb90226ceaab3c06782b1f1b5ea0cd1d8
ruffcoins <nzubeanthony4@gmail.com>
2022-07-21 18:03:01 +0100

    add beneficiary header

--- src/components/app/beneficiary/AddBeneficiary.jsx
+++ src/components/app/beneficiary/AddBeneficiary.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import {useNavigate} from "react-router-dom";
-import {ChevronLeftIcon} from "@heroicons/react/outline";
+import {Link, useNavigate} from "react-router-dom";
+import {ChevronLeftIcon, UserAddIcon, UserCircleIcon} from "@heroicons/react/outline";
 
 const AddBeneficiary = () => {
 	const navigate = useNavigate();
 
 	return (
 		<div className="lg:p-20 md:p-10 p-3">
-			<button className="flex flex-row items-center justify-start h-10 border-0 bg-transparent text-slate-500 md:mb-20 md:mt-0 my-10" onClick={() => navigate("/beneficiaries")}>
+			<button className="flex flex-row items-center justify-start h-10 border-0 bg-transparent text-slate-500 md:mb-20 md:mt-0 my-8" onClick={() => navigate("/beneficiaries")}>
 				<ChevronLeftIcon className="w-6" /> <p className="text-lg px-5">Back to Beneficiaries</p>
 			</button>
 			<div className="flex md:justify-start justify-center md:items-start items-center">
 				<div className="md:flex-1">
-					<form className="mb-0 space-y-0" action="src/components/website/globals/SignUpForm#" method="POST">
+
+					<div className="flex justify-between items-center h-24 bg-ihs-green-shade-50 rounded-md shadow-sm text-gray-600">
+						<div className="flex">
+							<UserAddIcon className="md:w-14 w-8 md:ml-10 ml-3" />
+							<h3 className="md:text-3xl text-2xl py-8 md:px-8 px-2">Add Beneficiary</h3>
+						</div>
+					</div>
+
+					<form className="my-16 space-y-0" action="src/components/website/globals/SignUpForm#" method="POST">
 
 						{/*First Name and last Name*/}
 						<div className="flex md:flex-row flex-col">
