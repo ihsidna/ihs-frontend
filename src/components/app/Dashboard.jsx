11195673b1c90cb78abd3a177fdfc440ff6d0a46
ruffcoins <nzubeanthony4@gmail.com>
2022-08-17 18:15:01 +0100

    bugfix - remove nested react links

--- src/components/app/Dashboard.jsx
+++ src/components/app/Dashboard.jsx
@@ -114,9 +114,7 @@ const Dashboard = () => {
 									<td
 										className="px-6 py-4 md:text-lg text-base leading-5 text-ihs-green whitespace-no-wrap border-b border-gray-200">
 										<Link to="/beneficiaries/viewbeneficiary">
-											<Link to="/beneficiaries/viewbeneficiary">
 											View Details
-										</Link>
 										</Link>
 									</td>
 
