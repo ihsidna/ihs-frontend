004ed687bae783e86602b351f9247075bd379d03
ruffcoins <nzubeanthony4@gmail.com>
2022-08-25 11:53:50 +0100

    feature - add loggedin user details and beneficiaries to global state

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -5,9 +5,11 @@ const AuthContext = createContext({});
 export const AuthProvider = ({children}) => {
 	const [auth, setAuth] = useState({});
 	const [persist, setPersist] = useState(JSON.parse(localStorage.getItem('persist' )) || true);
+	const [loggedInUser, setLoggedInUser] = useState({});
+	const [beneficiaries, setBeneficiaries] = useState([]);
 
 	return (
-		<AuthContext.Provider value={{auth, setAuth, persist, setPersist}}>
+		<AuthContext.Provider value={{auth, setAuth, persist, setPersist, loggedInUser, setLoggedInUser, beneficiaries, setBeneficiaries}}>
 			{children}
 		</AuthContext.Provider>
 	);
