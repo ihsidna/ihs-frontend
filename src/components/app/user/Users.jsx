79458e79a3d00d26d6a9d30ede628b70e1188c64
ruffcoins <nzubeanthony4@gmail.com>
2022-09-05 09:52:32 +0100

    feature - restrict adding admin and employee users to admins only

--- src/components/app/user/Users.jsx
+++ src/components/app/user/Users.jsx
@@ -1,25 +1,32 @@
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, useNavigate} from "react-router-dom";
 import ViewUser from "./ViewUser";
 import UserTable from "./UserTable";
-
+import AddUser from "./AddUser";
+import {userRoles} from "../../../data/enums";
+import useAuth from "../../../hooks/useAuth";
 
 const Users = () => {
 	return (
 		<Routes>
 			<Route index element={<ParentContent />} />
 			<Route path="/viewuser" element={<ViewUser />} />
+			<Route path="/adduser" element={<AddUser />} />
 		</Routes>
 	);
 };
 
 const ParentContent = () => {
+	const navigate = useNavigate();
+	const {auth} = useAuth();
 
 	return (
 		<div className="lg:p-20 md:p-10 p-3">
 			{/*Users Section*/}
 			<div className="flex justify-between items-center mt-10">
 				<h2 className="md:text-2xl text-xl">Users</h2>
-				{/*<button className="py-3 md:px-4 px-2" onClick={() => navigate('/users/adduser')}>Add Beneficiary</button>*/}
+				{auth?.userType === userRoles.Admin &&
+					<button className="py-3 md:px-4 px-2" onClick={() => navigate('/users/adduser')}>Add Admin User</button>
+				}
 			</div>
 
 			<hr className="my-10"/>
