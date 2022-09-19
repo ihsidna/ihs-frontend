72f9a2ee9b879f473f443bcdb49e501ef8b9e55c
ruffcoins <nzubeanthony4@gmail.com>
2022-09-19 22:19:01 +0100

    feature - delete service popup

--- src/components/app/service/ViewService.jsx
+++ src/components/app/service/ViewService.jsx
@@ -12,6 +12,11 @@ const ViewService = () => {
 	const {services} = useAuth();
 	const [serviceDetails, setServiceDetails] = useState({})
 	const [loading, setLoading] = useState(false);
+	const [toggleModal, setToggleModal] = useState(false);
+
+	const clicked = () => {
+		setToggleModal(true)
+	}
 
 	useEffect(() => {
 		const serviceId = service.serviceId;
@@ -55,8 +60,9 @@ const ViewService = () => {
 							<h3 className="md:text-3xl text-2xl py-8 md:px-8 px-2">Service Details</h3>
 						</div>
 
-						<h3 className="text-red-600 hover:text-red-700 text-xl md:px-8 px-3 hover:underline" onClick={deleteService}>Delete</h3>
-
+						<div className="pr-3">
+							<button className="text-xl md:px-8 px-3" onClick={clicked}>Delete</button>
+						</div>
 					</div>
 
 					<div className="mt-10 text-gray-600 md:text-xl" >
@@ -72,8 +78,60 @@ const ViewService = () => {
 					</div>
 				</div>
 			</div>
+			{toggleModal && <DeleteModal setToggleModal={setToggleModal} deleteFunction={deleteService}/>}
 		</div>
 	);
 };
 
+const DeleteModal = ({ setToggleModal, deleteFunction}) => {
+const navigate = useNavigate();
+const message = "Are you sure you want to delete this service? This action cannot be undone."
+
+	const handleDelete = () => {
+		deleteFunction().then(
+			navigate(-1)
+		);
+	}
+	const handleCancelClick = () => {
+		setToggleModal(false)
+	}
+
+	return (
+		<div className="bg-zinc-200 opacity-90 fixed inset-0 z-50   ">
+
+			<div className="flex h-screen justify-center items-center">
+				<div className="w-full md:w-1/3 mx-auto">
+					<div className=" flex flex-col p-5 rounded-lg shadow bg-white">
+						<div className="flex flex-col items-center text-center">
+							<div className="inline-block p-4 bg-ihs-green-shade-50 rounded-full">
+								<svg className="w-12 h-12 fill-current text-ihs-green" xmlns="http://www.w3.org/2000/svg"
+										 viewBox="0 0 24 24">
+									<path d="M0 0h24v24H0V0z" fill="none"/>
+									<path d="M12 5.99L19.53 19H4.47L12 5.99M12 2L1 21h22L12 2zm1 14h-2v2h2v-2zm0-6h-2v4h2v-4z"/>
+								</svg>
+							</div>
+							<h2 className="mt-2 md:text-2xl font-semibold text-gray-800">Are You Sure?</h2>
+							<p className="mt-2 md:text-xl text-gray-600 leading-relaxed">{message}</p>
+						</div>
+
+						<div className="flex items-center mt-3">
+							<button
+								className="flex-1 px-4 py-2 bg-gray-100 text-ihs-green md:text-lg text-sm font-medium rounded-md"
+								onClick={handleCancelClick}>
+								Cancel
+							</button>
+
+							<button
+								className="flex-1 px-4 py-2 ml-2 text-white md:text-lg text-sm font-medium rounded-md"
+								onClick={handleDelete}>
+								Yes
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	);
+}
 export default ViewService;
