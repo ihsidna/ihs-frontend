1715c20e5edf0352ce32ca511883a92d05a35fa3
ruffcoins <nzubeanthony4@gmail.com>
2022-09-22 12:46:43 +0100

    feature - add page title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,17 +5,26 @@ import Services from "../components/website/landing/Services";
 import WhyUs from "../components/website/landing/WhyUs";
 import Navbar from "../components/website/globals/Navbar";
 import Footer from "../components/website/globals/Footer";
+import {Helmet, HelmetProvider} from "react-helmet-async";
 
 const Home = () => {
 	return (
-		<>
-			<Navbar />
-			<Hero />
-			<HowItWorks />
-			<Services />
-			<WhyUs />
-			<Footer />
-		</>
+		<HelmetProvider>
+			<>
+				<Helmet>
+					<title>Home | IHS | Primary and Secondary Healthcare</title>
+					<link rel="canonical" href="https://www.ihsmdinc.com/" />
+				</Helmet>
+				<>
+					<Navbar />
+					<Hero />
+					<HowItWorks />
+					<Services />
+					<WhyUs />
+					<Footer />
+				</>
+			</>
+		</HelmetProvider>
 	);
 };
 
