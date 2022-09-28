f205b38eb91a1fd282325b05c2c0b22b700fb04d
ruffcoins <nzubeanthony4@gmail.com>
2022-09-28 21:05:20 +0100

    feature - test google analytics again

---
+++ src/hooks/useAnalytics.js
@@ -0,0 +1,26 @@
+import React, {useEffect} from 'react'
+import { useLocation } from 'react-router-dom'
+import { useDetectAdBlock } from 'adblock-detect-react'
+
+
+import * as analytics from '../data/ga4'
+
+export function useAnalytics() {
+	const location = useLocation()
+	const path = location.pathname + location.search
+
+	const adBlockDetected = useDetectAdBlock()
+
+	useEffect(() => {
+		if(!adBlockDetected) analytics.init()
+	}, [])
+
+	useEffect(() => {
+		if (!adBlockDetected) {
+			analytics.sendPageview(path)
+		}
+	}, [adBlockDetected, path]);
+
+}
+
+export default useAnalytics
