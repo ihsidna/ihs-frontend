e7ef7f4d861898e1750cf30f1dfd0f70a48fc9cc
Anthony Chukwujekwu <nzubeanthony4@gmail.com>
2022-10-01 10:25:38 +0100

    refactor - update with ihs cloud function url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// const DEV_URL = 'http://localhost:5001/company-web-projects/us-central1/ihsApiv1';
-const PROD_URL = 'https://us-central1-company-web-projects.cloudfunctions.net/ihsApiv1';
+// const DEV_URL = 'http://localhost:5001/ihs-project/us-central1/ihsApiv1';
+const PROD_URL = 'https://us-central1-ihs-project.cloudfunctions.net/ihsApiv1';
 export default axios.create({
 	baseURL: PROD_URL
 });
