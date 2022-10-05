340b57fc14ecedb8645b3483da8d27313cb940b5
Anthony Chukwujekwu <nzubeanthony4@gmail.com>
2022-10-05 09:34:50 +0100

    chore - comment out IHS google analytics configuration to avoid wrong analytics data

--- src/hooks/useAnalytics.js
+++ src/hooks/useAnalytics.js
@@ -1,26 +1,28 @@
-import React, {useEffect} from 'react'
-import { useLocation } from 'react-router-dom'
-import { useDetectAdBlock } from 'adblock-detect-react'
+// todo: uncomment before a pull request
 
+// import React, {useEffect} from 'react'
+// import { useLocation } from 'react-router-dom'
+// import { useDetectAdBlock } from 'adblock-detect-react'
+//
+//
+// import * as analytics from '../data/ga4'
+//
+// export function useAnalytics() {
+// 	const location = useLocation()
+// 	const path = location.pathname + location.search
+//
+// 	const adBlockDetected = useDetectAdBlock()
+//
+// 	useEffect(() => {
+// 		if(!adBlockDetected) analytics.init()
+// 	}, [])
+//
+// 	useEffect(() => {
+// 		if (!adBlockDetected) {
+// 			analytics.sendPageview(path)
+// 		}
+// 	}, [adBlockDetected, path]);
+//
+// }
 
-import * as analytics from '../data/ga4'
-
-export function useAnalytics() {
-	const location = useLocation()
-	const path = location.pathname + location.search
-
-	const adBlockDetected = useDetectAdBlock()
-
-	useEffect(() => {
-		if(!adBlockDetected) analytics.init()
-	}, [])
-
-	useEffect(() => {
-		if (!adBlockDetected) {
-			analytics.sendPageview(path)
-		}
-	}, [adBlockDetected, path]);
-
-}
-
-export default useAnalytics
+// export default useAnalytics
