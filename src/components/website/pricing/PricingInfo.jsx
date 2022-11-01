80dc16f4cbd63b1d5e946a175f14780271ec12fc
Anthony Chukwujekwu <nzubeanthony4@gmail.com>
2022-11-01 10:56:28 +0100

    refactor - update pricing copy

--- src/components/website/pricing/PricingInfo.jsx
+++ src/components/website/pricing/PricingInfo.jsx
@@ -26,11 +26,11 @@ const PricingDetails = () => {
 				<div className="grid gap-6 mt-16 sm:gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
 					<div
 							 className="px-6 py-4 transition-colors duration-200 transform rounded-lg hover:bg-ihs-green-shade-50 border shadow">
-						<p className="text-lg font-medium text-gray-800">Bi-Weekly Coverage</p>
+						<p className="text-lg font-medium text-gray-800">Bi-Weekly Payment</p>
 						<h4 className="mt-2 md:text-3xl text-lg font-semibold text-gray-800 ">
 							$50
 							<span className="font-light text-sm text-gray-600 capitalize">
-								/ Beneficiary / 2 Weeks
+								/ Beneficiary
 							</span>
 						</h4>
 
@@ -44,9 +44,9 @@ const PricingDetails = () => {
 
 					<div
 						className="px-6 py-4 transition-colors duration-200 transform rounded-lg hover:bg-ihs-green-shade-50 border shadow">
-						<p className="text-lg font-medium text-gray-800">Monthly Coverage</p>
+						<p className="text-lg font-medium text-gray-800">Monthly Payment</p>
 						<h4 className="mt-2 md:text-3xl text-lg font-semibold text-gray-800 ">$100 <span
-							className="font-light text-sm text-gray-600 capitalize">/ Beneficiary / Month
+							className="font-light text-sm text-gray-600 capitalize">/ Beneficiary
 						</span></h4>
 
 						<Link to="/signup">
@@ -59,9 +59,9 @@ const PricingDetails = () => {
 
 					<div
 						className="px-6 py-4 transition-colors duration-200 transform rounded-lg hover:bg-ihs-green-shade-50 border shadow">
-						<p className="text-lg font-medium text-gray-800">Yearly Coverage</p>
+						<p className="text-lg font-medium text-gray-800">Yearly Payment</p>
 						<h4 className="mt-2 md:text-3xl text-lg font-semibold text-gray-800 ">$1200 <span
-							className="font-light text-sm text-gray-600 capitalize">/ Beneficiary / Year
+							className="font-light text-sm text-gray-600 capitalize">/ Beneficiary
 						</span></h4>
 
 						<Link to="/signup">
