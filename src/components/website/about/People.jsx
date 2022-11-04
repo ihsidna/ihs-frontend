85d18674b13efdf5bc45c5c0ab03f3daa86ab3a8
Anthony Chukwujekwu <nzubeanthony4@gmail.com>
2022-11-04 21:48:32 +0100

    refactor - include teams' actual images

--- src/components/website/about/People.jsx
+++ src/components/website/about/People.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import AvatarImage from "../../../assets/images/avatar.jpg";
+import Boye from "../../../assets/images/Boye.jpg";
+import Caleb from "../../../assets/images/Caleb.jpg";
+import Michael from "../../../assets/images/Michael.jpg";
+import Esther from "../../../assets/images/Esther.jpg";
+import Olakanmi from "../../../assets/images/Olakanmi.jpg";
+import Dapo from "../../../assets/images/Dapo.jpg";
+import Zainab from "../../../assets/images/Zainab.jpg";
+import Moyo from "../../../assets/images/Moyo.jpeg";
+import Tobi from "../../../assets/images/Tobi.jpg";
 
 const People = () => {
 	return (
@@ -14,9 +22,9 @@ const People = () => {
 					<div className="my-10">
 						<div className="flex justify-center md:mx-20 mx-5">
 							<div className="grid lg:grid-cols-3 md:grid-cols-2">
-								<div className="shadow-md md:mx-4 my-6 bg-white bg-white">
+								<div className="shadow-md md:mx-4 my-6 bg-white">
 									<div>
-										<img src={AvatarImage} alt="Secondary & tertiary Care" className="rounded-t-lg shadow-md" />
+										<img src={Olakanmi} alt="Secondary & tertiary Care" className="rounded-t-lg shadow-md w-full" />
 									</div>
 									<div className="bg-white md:py-10 py-8 -mt-1 rounded-b-lg">
 										<div className="flex flex-col mx-4">
@@ -29,7 +37,7 @@ const People = () => {
 								</div>
 								<div className="shadow-md md:mx-4 my-6 bg-white">
 									<div>
-										<img src={AvatarImage} alt="Secondary & tertiary Care" className="rounded-t-lg shadow-md" />
+										<img src={Esther} alt="Secondary & tertiary Care" className="rounded-t-lg shadow-md w-full" />
 									</div>
 									<div className="bg-white md:py-10 py-8 -mt-1 rounded-b-lg">
 										<div className="flex flex-col mx-4">
@@ -42,7 +50,7 @@ const People = () => {
 								</div>
 								<div className="shadow-md md:mx-4 my-6 bg-white">
 									<div>
-										<img src={AvatarImage} alt="Secondary & tertiary Care" className="rounded-t-lg shadow-md" />
+										<img src={Tobi} alt="Secondary & tertiary Care" className="rounded-t-lg shadow-md w-full" />
 									</div>
 									<div className="bg-white md:py-10 py-8 -mt-1 rounded-b-lg">
 										<div className="flex flex-col mx-4">
@@ -55,7 +63,7 @@ const People = () => {
 								</div>
 								<div className="shadow-md md:mx-4 my-6 bg-white">
 									<div>
-										<img src={AvatarImage} alt="Secondary & tertiary Care" className="rounded-t-lg shadow-md" />
+										<img src={Dapo} alt="Secondary & tertiary Care" className="rounded-t-lg shadow-md w-full" />
 									</div>
 									<div className="bg-white md:py-10 py-8 -mt-1 rounded-b-lg">
 										<div className="flex flex-col mx-4">
@@ -68,7 +76,7 @@ const People = () => {
 								</div>
 								<div className="shadow-md md:mx-4 my-6 bg-white">
 									<div>
-										<img src={AvatarImage} alt="Secondary & tertiary Care" className="rounded-t-lg shadow-md" />
+										<img src={Caleb} alt="Secondary & tertiary Care" className="rounded-t-lg shadow-md w-full" />
 									</div>
 									<div className="bg-white md:py-10 py-8 -mt-1 rounded-b-lg">
 										<div className="flex flex-col mx-4">
@@ -81,7 +89,7 @@ const People = () => {
 								</div>
 								<div className="shadow-md md:mx-4 my-6 bg-white">
 									<div>
-										<img src={AvatarImage} alt="Secondary & tertiary Care" className="rounded-t-lg shadow-md" />
+										<img src={Michael} alt="Secondary & tertiary Care" className="rounded-t-lg shadow-md w-full" />
 									</div>
 									<div className="bg-white md:py-10 py-8 -mt-1 rounded-b-lg">
 										<div className="flex flex-col mx-4">
@@ -94,7 +102,7 @@ const People = () => {
 								</div>
 								<div className="shadow-md md:mx-4 my-6 bg-white">
 									<div>
-										<img src={AvatarImage} alt="Secondary & tertiary Care" className="rounded-t-lg shadow-md" />
+										<img src={Boye} alt="Secondary & tertiary Care" className="rounded-t-lg shadow-md w-full" />
 									</div>
 									<div className="bg-white md:py-10 py-8 -mt-1 rounded-b-lg">
 										<div className="flex flex-col mx-4">
@@ -107,7 +115,7 @@ const People = () => {
 								</div>
 								<div className="shadow-md md:mx-4 my-6 bg-white">
 									<div>
-										<img src={AvatarImage} alt="Secondary & tertiary Care" className="rounded-t-lg shadow-md" />
+										<img src={Zainab} alt="Secondary & tertiary Care" className="rounded-t-lg shadow-md w-full" />
 									</div>
 									<div className="bg-white md:py-10 py-8 -mt-1 rounded-b-lg">
 										<div className="flex flex-col mx-4">
@@ -120,7 +128,7 @@ const People = () => {
 								</div>
 								<div className="shadow-md md:mx-4 my-6 bg-white">
 									<div>
-										<img src={AvatarImage} alt="Secondary & tertiary Care" className="rounded-t-lg shadow-md" />
+										<img src={Moyo} alt="Secondary & tertiary Care" className="rounded-t-lg shadow-md w-full" />
 									</div>
 									<div className="bg-white md:py-10 py-8 -mt-1 rounded-b-lg">
 										<div className="flex flex-col mx-4">
