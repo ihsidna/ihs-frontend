928918995157a2451254b4c4678f909bf577faad
ruffcoins <nzubeanthony4@gmail.com>
2022-11-18 16:12:48 +0100

    refactor - refactor responsive sidebar and update npm packages security vulnerabilities

--- src/components/app/Layout.jsx
+++ src/components/app/Layout.jsx
@@ -1,165 +1,33 @@
-import React, {useState} from 'react';
-import {Link, NavLink, Outlet, useNavigate} from "react-router-dom";
+import React, {Fragment} from 'react';
+import {Outlet} from "react-router-dom";
 import Logo from "../../assets/images/logo.svg";
-import {LogoutIcon, MenuIcon, XIcon} from "@heroicons/react/outline";
-import {footerLinks, userLinks, adminLinks, employeeLinks} from "../../data/data";
 import useAuth from "../../hooks/useAuth";
-import axios from "../../api/axios";
-import {avatar, userRoles} from "../../data/enums";
+import {avatar} from "../../data/enums";
 import Avatar from "react-avatar";
-import Modal from "./Modal";
+import Sidebar from "./Sidebar";
 
 const Layout = () => {
-	const navigate = useNavigate();
-	const {auth, setAuth, loggedInUser} = useAuth();
-	const [toggleModal, setToggleModal] = useState(false)
-
-	const clicked = () => {
-		setToggleModal(true)
-	}
-
-	// sidebar is active on init
-	const [sidebar, setSidebar] = useState(true);
-
-	// mobile screen is inactive on init
-	const [mobile, setMobile] = useState(false);
-
-	const toggleSidebar = () => {
-		setSidebar((prevSidebar) => !prevSidebar);
-	}
-	const toggleSidebarOnMobile = () => {
-		if (window.innerWidth < 768){
-			toggleSidebar();
-		}
-	}
-	const handleMobile = () => {
-		if (window.innerWidth < 768){
-			setMobile(true);
-			toggleSidebar();
-		}
-	};
-
-	const scrollToTop = window.scrollTo({top: 0, left: 0, behavior: 'auto'});
-
-	const activeLink = "flex w-80 items-center gap-5 text-lg text-ihs-green bg-ihs-green-shade-200 border border-0 border-r-2 border-r-ihs-green pl-7 py-2"
-	const normalLink = "flex w-80 items-center gap-5 text-lg hover:bg-ihs-green-shade-100 border border-0 hover:border-r-2 hover:border-r-ihs-green pl-7 py-2"
-	const sidebarStyle = "drop-shadow-2xl md:w-80 w-full sticky left-0 top-0 border border-0 border-r border-slate-200 bg-cyan-50 h-screen md:overflow-hidden overflow-auto md:hover:overflow-hidden pb-10 z-10"
-
-	const logout = async () => {
-		setAuth({});
-		await axios('/logout', {
-			withCredentials: true
-		});
-
-		localStorage.clear();
-
-		navigate('/signin');
-	}
+	const {loggedInUser} = useAuth();
 
 	return (
-		<div>
-			<div className="flex">
-				<div>
-					{/*Sidebar*/}
-					{/*Show the sidebar by default.*/}
-					{/*If the sidebar value is false, hide the sidebar.*/}
-					{/*If mobile state is true, hide the sidebar using the handleMobile function*/}
-					{/*if it is not a mobile device, show the sidebar.*/}
-					<div className={`${sidebarStyle} ${!sidebar ? "hidden" : !mobile ? handleMobile() : sidebarStyle}` }>
-						<div className="flex justify-center items-center px-4">
-							<Link to="/dashboard" onClick={() => {
-							}} className="items-center my-4 flex">
-								<img src={Logo} alt="logo" className="w-40" />
-							</Link>
+		<div className="flex">
+			<Sidebar />
+			<div className="w-full">
+				<nav className="flex justify-between h-20 border border-0 border-b border-slate-200 bg-white sm:pl-32 pl-20">
+					<p></p>
+					<img src={Logo} alt="logo" className="w-44" />
+
+					<div className="flex flex-row items-center">
+						<p className="text-xl text-gray-700 hidden md:block">{loggedInUser?.firstName} {loggedInUser?.lastName}</p>
+						<div className="px-5">
+							<Avatar name={`${loggedInUser?.firstName} ${loggedInUser?.lastName}`} color={avatar.BackgroundColor} fgColor={avatar.ForegroundColor}  size={avatar.width} round={true}/>
 						</div>
-						<div className="mt-5">
-							{ auth?.userType === userRoles.User
-								?
-									userLinks.map((item) => (
-										<NavLink to={`/${item.path}`} key={item.path} onClick={() => {toggleSidebarOnMobile(); return scrollToTop} } className={({isActive}) => isActive ? activeLink : normalLink}>
-											{item.icon}
-											<span className="capitalize">{item.title}</span>
-										</NavLink>
-									))
-								:
-									auth?.userType === userRoles.Employee
-									?
-										employeeLinks.map((item) => (
-											<NavLink to={`/${item.path}`} key={item.path} onClick={() => {toggleSidebarOnMobile(); return scrollToTop} } className={({isActive}) => isActive ? activeLink : normalLink}>
-												{item.icon}
-												<span className="capitalize">{item.title}</span>
-											</NavLink>
-										))
-									:
-										adminLinks.map((item) => (
-											<NavLink to={`/${item.path}`} key={item.path} onClick={() => {toggleSidebarOnMobile(); return scrollToTop} } className={({isActive}) => isActive ? activeLink : normalLink}>
-												{item.icon}
-												<span className="capitalize">{item.title}</span>
-											</NavLink>
-										))
-							}
-						</div>
-
-						{/*Footer*/}
-						<div className="border border-0 border-t border-slate-200 fixed bottom-2 py-4">
-							{footerLinks.map((item) => (
-								<NavLink to={`/${item.path}`}
-								 key={item.path}
-								 onClick={() => toggleSidebarOnMobile() }
-								 className={({isActive}) => isActive ? activeLink : normalLink}
-								>
-									{item.icon}
-									<span className="capitalize">{item.title}</span>
-								</NavLink>
-							))}
-
-							<div className={`cursor-pointer ${normalLink}`}  onClick={clicked}>
-								<LogoutIcon  className="w-6" />
-								<span className="capitalize">Logout</span>
-							</div>
-
-								<p className="pl-7 py-4 text-sm">© 2022 Copyright. v1.0.0</p>
-						</div>
-						{/*	End Footer*/}
-
 					</div>
-				{/*	End Sidebar*/}
-				</div>
-
-				<div className=" overflow-x-hidden grow w-screen">
-
-					{/*Navbar*/}
-					<nav className="flex justify-between h-20 border border-0 border-b border-slate-200 sticky top-0 bg-white">
-						<button onClick={() => {toggleSidebar()}} className="md:invisible bg-transparent text-slate-500 text-xl border border-slate-300 hover:border-ihs-green-shade-100 m-4 p-2 block">
-							{ !sidebar ? <MenuIcon className="w-6"/> : <XIcon className="w-6"/> }
-						</button>
-
-						<Link to="/dashboard" onClick={() => {
-						}} className={`"md:hidden" ${sidebar ? "hidden" : "items-center my-4 flex"}`}>
-							<img src={Logo} alt="logo" className="w-40" />
-						</Link>
-
-						<div className="flex flex-row items-center">
-							<div className=" ">
-								<p className="text-xl text-gray-700 hidden md:block">{loggedInUser?.firstName} {loggedInUser?.lastName}</p>
-							</div>
-							<div className={` ${sidebar ? "hidden md:block px-5" : "px-5"}`}>
-								<Avatar name={`${loggedInUser?.firstName} ${loggedInUser?.lastName}`} color={avatar.BackgroundColor} fgColor={avatar.ForegroundColor}  size={avatar.width} round={true}/>
-							</div>
-						</div>
-
-					</nav>
-					{/*End Navbar*/}
-
-					{/*when sidebar is active, dashboard content is hidden*/}
-					<div className={`"block" ${!mobile ? "block" : sidebar ? "hidden" : "block"}`}>
-						<Outlet />
-					</div>
-
-				</div>
-
+				</nav>
+				<Fragment className="w-screen">
+					<Outlet />
+				</Fragment>
 			</div>
-			{toggleModal && <Modal setToggleModal={setToggleModal} executeFunction={logout} message="Are you sure you want to logout?" header='Are You Sure?' /> }
 		</div>
 	);
 };
---
+++ src/components/app/Sidebar.jsx
@@ -0,0 +1,99 @@
+import {useState} from 'react';
+import {LogoutIcon, ViewListIcon, XIcon} from "@heroicons/react/outline";
+import Logo from "../../assets/images/logo.svg";
+import {userRoles} from "../../data/enums";
+import {adminLinks, employeeLinks, footerLinks, userLinks} from "../../data/data";
+import {NavLink, useNavigate} from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import Modal from "./Modal";
+import axios from "../../api/axios";
+
+const activeLink = "flex w-70 items-center gap-5 text-lg text-ihs-green bg-ihs-green-shade-200 border border-0 border-r-2 border-r-ihs-green pl-5 py-2"
+const normalLink = "flex w-70 items-center gap-5 text-lg hover:bg-ihs-green-shade-100 border border-0 hover:border-r-2 hover:border-r-ihs-green pl-5 py-2"
+
+const Sidebar = () => {
+	const {auth, setAuth} = useAuth();
+	const navigate = useNavigate();
+	const [toggleModal, setToggleModal] = useState(false)
+
+	const showModal = () => {
+		setToggleModal(true)
+	}
+
+	const logout = async () => {
+		setAuth({});
+		await axios('/logout', {
+			withCredentials: true
+		});
+
+		localStorage.clear();
+
+		navigate('/signin');
+	}
+
+	const displayLinks = (links) => {
+		return (
+			links.map((item) => {
+				return(
+					<NavLink to={`/${item.path}`} key={item.path} className={({isActive}) => isActive ? activeLink : normalLink} onClick={openBar}>
+						{item.icon}
+						<span className="capitalize">{item.title}</span>
+					</NavLink>
+				)
+			} )
+		)
+
+	}
+
+	const openBar = () => {
+		document.querySelector('.sidebar').classList.toggle('left-[-300px]')
+	}
+
+	return (
+		<>
+			<div>
+				<span className="absolute text-white text-4xl top-5 left-4 cursor-pointer" onClick={openBar}>
+
+					<ViewListIcon className="w-10 h-10 px-2 bg-ihs-green text-white rounded-md shadow-xl"/>
+				</span>
+				<div className="sidebar fixed top-0 bottom-0 left-0 left-[-300px] duration-1000
+				 w-[300px] overflow-y-auto text-center bg-cyan-50 shadow h-screen">
+						<div>
+							<div className="p-2.5 mt-1 flex items-center rounded-md ">
+								<img src={Logo} alt="logo" className="w-40" />
+								<XIcon className="ml-20 text-slate-500 w-10 h-10 bg-transparent border" onClick={openBar}/>
+							</div>
+							<hr className="my-2 text-gray-600" />
+
+							<div>
+
+								{ auth?.userType === userRoles.User
+									? displayLinks(userLinks)
+									: auth?.userType === userRoles.Employee
+									? displayLinks(employeeLinks)
+									: displayLinks(adminLinks)
+								}
+
+
+								<div className="fixed bottom-0 py-4">
+
+									{displayLinks(footerLinks)}
+
+									<div className={`cursor-pointer ${normalLink}`}  onClick={showModal}>
+										<LogoutIcon  className="w-6" />
+										<span className="capitalize">Logout</span>
+									</div>
+
+									<p className="flex justify-start pl-5 py-2 text-sm">© 2022 Copyright. v1.1.0</p>
+								</div>
+
+							</div>
+						</div>
+					</div>
+				{toggleModal && <Modal setToggleModal={setToggleModal} executeFunction={logout} message="Are you sure you want to logout?" header='Are You Sure?' /> }
+			</div>
+		</>
+	);
+};
+
+export default Sidebar;
