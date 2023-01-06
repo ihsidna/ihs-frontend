b8746508adf4dfcb82c949de2e15b30ded7cac3a
ihsidna <ihsidnadev@gmail.com>
2023-01-06 12:04:59 +0100

    refactor(Registeration): Fix Registration form
    - Add regex/ validation  to input fields e.g first, last names, password to make sure that it cannot be exploited with links or special characters.
    
    - Provide appropriate error messages for input fields when the validation fails e.g “First name cannot contain @ or special characters or .com“
    
    - Put eye icon to show password
    
    BREAKING CHANGE: None

--- src/utils/formSchema.js
+++ src/utils/formSchema.js
@@ -6,5 +6,5 @@ export const signupSchema = yup.object().shape({
 	lastName: yup.string().matches(validName, {message: "Minimum of 3 characters and must not contain special characters"}).required("Last name is required"),
 	email: yup.string().email("Please enter a valid email").required("Email is required"),
 	phone: yup.string().min(6).required("Phone number is required"),
-	password: yup.string().min(6).matches(validPassword, { message: "Minimum of 6 characters. Must contain 1 uppercase, 1 lowercase and 1 number" }).required("Password is required"),
+	password: yup.string().min(6).matches(validPassword, { message: "Minimum of 6 characters. Must contain 1 uppercase letter, 1 lowercase letter and 1 number" }).required("Password is required"),
 });
