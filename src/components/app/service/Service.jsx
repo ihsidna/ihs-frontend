cb4c0cdd9bd2484aa27532f9cbc1c74e95984e55
ihsidna <106628619+ihsidna@users.noreply.github.com>
2023-02-11 17:13:36 +0100

    feature(Services): Create service modal
    - Convert add service page to modal that shows when add service button is clicked.
    - On save service, update the services list to show added service.
    
    BREAKING CHANGE: None

--- src/components/app/service/Service.jsx
+++ src/components/app/service/Service.jsx
@@ -1,5 +1,5 @@
 import React, {useCallback, useEffect, useState} from 'react';
-import {Route, Routes, useNavigate} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import AddService from "./AddService";
 import ViewService from "./ViewService";
 import ServiceTable from "./ServiceTable";
@@ -7,6 +7,7 @@ import {Helmet, HelmetProvider} from "react-helmet-async";
 import useAxiosPrivate from "../../../hooks/useAxiosPrivate";
 import useAuth from "../../../hooks/useAuth";
 import TopBarProgress from "react-topbar-progress-indicator";
+import AddServiceModal from "./AddServiceModal";
 
 TopBarProgress.config({
 	barColors: {
@@ -27,9 +28,11 @@ const Service = () => {
 
 const ParentContent = () => {
 	const {services, setServices} = useAuth();
-	const navigate = useNavigate();
 	const axiosPrivate = useAxiosPrivate();
 	const [loading, setLoading] = useState();
+
+	const [showAddServiceModal, setShowAddServiceModal] = useState(false);
+	const [addServiceModalSuccess, setAddServiceModalSuccess] = useState(false);
 
 	const getServices = useCallback( async () => {
 		const response = await axiosPrivate.get(
@@ -43,12 +46,23 @@ const ParentContent = () => {
 			getServices().then(() => {
 				setLoading(false)
 			});
-	}, [getServices]);
+	}, [getServices, addServiceModalSuccess]);
 
+	const handleShowAddServiceModal = () => {
+		setShowAddServiceModal(true);
+	}
 
 	return (
 		<HelmetProvider>
 			{loading && <TopBarProgress />}
+
+			{/*	show modal if modal is toggled*/}
+			{showAddServiceModal && <AddServiceModal
+				setShowAddServiceModal={setShowAddServiceModal}
+				addServiceModalSuccess={addServiceModalSuccess}
+				setAddServiceModalSuccess={setAddServiceModalSuccess}
+			/>}
+
 			<>
 				<Helmet>
 					<title>Services | IHS Dashboard</title>
@@ -57,7 +71,7 @@ const ParentContent = () => {
 				<div className="lg:p-20 md:p-10 p-3">
 				<div className="flex justify-between items-center md:mt-16 mt-20">
 					<h2 className="md:text-2xl text-xl">All Services</h2>
-					<button className="py-3 md:px-4 px-2" onClick={() => navigate('/servicess/addservice')}>Add Service</button>
+					<button className="py-3 md:px-4 px-2" onClick={handleShowAddServiceModal}>Add Service</button>
 				</div>
 
 				<hr className="my-10"/>
