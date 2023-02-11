17c1b0ea350d64e5ee4d80f044e89ad188e8f24d
ihsidna <106628619+ihsidna@users.noreply.github.com>
2023-02-11 17:26:03 +0100

    feature(Form Schema): Set form schema
    - Add New User form
    - Add health worker form
    - Add service form

--- src/utils/formSchema.js
+++ src/utils/formSchema.js
@@ -22,3 +22,27 @@ export const changePasswordSchema = yup.object().shape({
 export const changePhoneNumberSchema = yup.object().shape({
 	phone: yup.string().required("Phone Number is required"),
 })
+
+export const addServiceSchema = yup.object().shape({
+	name: yup.string().required("Service Name is required"),
+	category: yup.string().required("Service Category is required"),
+})
+
+export const addHealthWorkerSchema = yup.object().shape({
+	firstName: yup.string().matches(validName, {message: "Minimum of 3 characters and must not contain special characters"}).required("First name is required"),
+	lastName: yup.string().matches(validName, {message: "Minimum of 3 characters and must not contain special characters"}).required("Last name is required"),
+	email: yup.string().email("Please enter a valid email").required("Email is required"),
+	phone: yup.string().min(6).required("Phone number is required"),
+	qualification: yup.string().required("Qualification is required")
+})
+
+export const addUserSchema = yup.object().shape({
+	firstName: yup.string().matches(validName, {message: "Minimum of 3 characters and must not contain special characters"}).required("First name is required"),
+	lastName: yup.string().matches(validName, {message: "Minimum of 3 characters and must not contain special characters"}).required("Last name is required"),
+	email: yup.string().email("Please enter a valid email").required("Email is required"),
+	phone: yup.string().min(6).required("Phone number is required"),
+	password: yup.string().min(6).matches(validPassword, { message: "Minimum of 6 characters. Must contain 1 uppercase letter, 1 lowercase letter and 1 number" }).required("Password is required"),
+	role: yup.string().required('Role is required'),
+	dob: yup.string().required('Date of Birth is required')
+})
+
