a64338eff9e344c3de9f11f67ddfb6ef1cc18d91
Anthony Chukwujekwu <nzubeanthony4@gmail.com>
2023-05-25 12:31:00 +0100

    feature(Promotional Appointments):

--- src/components/app/appointment/BookAppointment.jsx
+++ src/components/app/appointment/BookAppointment.jsx
@@ -9,7 +9,7 @@ import TopBarProgress from "react-topbar-progress-indicator";
 import Modal from "../Modal";
 import {WATDateString} from "../../../hooks/useFormatDate";
 
-TopBarProgress.config({
+TopBarProgress.config ({
 	barColors: {
 		"0": "#05afb0"
 	},
@@ -19,102 +19,134 @@ TopBarProgress.config({
 const BOOK_APPOINTMENT = "/appointment/create"
 
 const BookAppointment = () => {
-	const axiosPrivate = useAxiosPrivate();
-	const navigate = useNavigate();
-	const {beneficiaries, services, setServices} = useAuth();
-
-	const [beneficiary, setBeneficiary] = useState('');
-	const [service, setService] = useState('');
-	const [date, setDate] = useState('');
-	const [time, setTime] = useState('');
-	const [loading, setLoading] = useState(false);
-
-	const [toggleModal, setToggleModal] = useState(false)
-
-	const clicked = () => {
-		setToggleModal(true)
-	}
-
+	const axiosPrivate = useAxiosPrivate ();
+	const navigate = useNavigate ();
+	const {beneficiaries, services, setServices} = useAuth ();
+	
+	const [beneficiary, setBeneficiary] = useState ('');
+	const [service, setService] = useState ('');
+	const [date, setDate] = useState ('');
+	const [time, setTime] = useState ('');
+	const [loading, setLoading] = useState (false);
+	
+	const [toggleModal, setToggleModal] = useState (false)
+	
+	// const clicked = () => {
+	// 	setToggleModal (true)
+	// }
+	
 	const handleSubmit = async (e) => {
-		e.preventDefault();
-		setLoading(true);
-
-		const appointmentDate = WATDateString(date);
+		e.preventDefault ();
+		setLoading (true);
+		
+		const appointmentDate = WATDateString (date);
 		
 		try {
+			// // normal workflow for booking appointments
+			
 			// verify beneficiary coverage subscription
-			const exactBeneficiary = beneficiaries.filter((ben) => ben.id === beneficiary);
+			// const exactBeneficiary = beneficiaries.filter ((ben) => ben.id === beneficiary);
+			
+			// if (exactBeneficiary[0]?.subscription?.status === 'active') {
+			// 	await axiosPrivate.post (BOOK_APPOINTMENT,
+			// 		JSON.stringify ({
+			// 			beneficiaryId: beneficiary,
+			// 			serviceId: service,
+			// 			date: appointmentDate,
+			// 			time,
+			// 			status: appointmentStatus.Booked
+			// 		}),
+			// 		{
+			// 			headers: {
+			// 				'Content-Type': 'application/json'
+			// 			},
+			// 			withCredentials: true
+			// 		}
+			// 	);
+			//
+			// 	setBeneficiary ('');
+			// 	setService ('');
+			// 	setDate ('');
+			// 	setTime ('');
+			//
+			// 	setLoading (false);
+			//
+			// 	navigate ('/appointments')
+			// } else {
+			// 	clicked ();
+			// }
+			
+			// July workflow for booking appointments
+			await axiosPrivate.post (BOOK_APPOINTMENT,
+				JSON.stringify ({
+					beneficiaryId: beneficiary,
+					serviceId: service,
+					date: appointmentDate,
+					time,
+					status: appointmentStatus.Booked
+				}),
+				{
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					withCredentials: true
+				}
+			);
+			
+			setBeneficiary ('');
+			setService ('');
+			setDate ('');
+			setTime ('');
 
-			if (exactBeneficiary[0]?.subscription?.status === 'active') {
-				await axiosPrivate.post(BOOK_APPOINTMENT,
-					JSON.stringify({
-						beneficiaryId: beneficiary, serviceId: service, date: appointmentDate, time, status: appointmentStatus.Booked
-					}),
-					{
-						headers: {
-							'Content-Type': 'application/json',
-						},
-						withCredentials: true
-					}
-				);
+			setLoading (false);
 
-				setBeneficiary('');
-				setService('');
-				setDate('');
-				setTime('');
-
-				setLoading(false);
-
-				navigate('/appointments')
-			} else {
-				clicked();
-			}
-
+			navigate ('/appointments')
+			
 		} catch (err) {
 			if (!err.response) {
-				console.error(err);
-				console.error('No Server Response')
-				setLoading(false);
+				console.error (err);
+				console.error ('No Server Response')
+				setLoading (false);
 			} else {
-				console.error(err)
+				console.error (err)
 			}
 		}
 	}
-
+	
 	const redirectToPricingPage = () => {
-		navigate(`/beneficiaries/updatebeneficiary/${beneficiary}/addhealthcoverage`);
+		navigate (`/beneficiaries/updatebeneficiary/${beneficiary}/addhealthcoverage`);
 	}
-
-	useEffect(() => {
-		setLoading(true)
+	
+	useEffect (() => {
+		setLoading (true)
 		let isMounted = true;
-		const controller = new AbortController();
-
+		const controller = new AbortController ();
+		
 		const getServices = async () => {
 			try {
-				const response = await axiosPrivate.get(
+				const response = await axiosPrivate.get (
 					"/admin/service/all",
 					{
 						signal: controller?.signal
 					});
-
-				isMounted && setServices(response.data.data);
-				localStorage.setItem("services", JSON.stringify(response.data.data))
-				setLoading(false)
+				
+				isMounted && setServices (response.data.data);
+				localStorage.setItem ("services", JSON.stringify (response.data.data))
+				setLoading (false)
 			} catch (err) {
-				console.error(err)
+				console.error (err)
 			}
 		}
-
-		getServices();
-
+		
+		getServices ();
+		
 		return () => {
 			isMounted = false;
-			controller.abort();
+			controller.abort ();
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
-
+	
 	return (
 		<HelmetProvider>
 			<>
@@ -126,23 +158,25 @@ const BookAppointment = () => {
 					{loading && <TopBarProgress/>}
 					<button
 						className="flex flex-row items-center justify-start h-10 border-0 bg-transparent text-slate-500 my-5 lg:mt-10"
-						onClick={() => navigate("/appointments")}>
-						<ChevronLeftIcon className="w-6"/> <p className="text-lg px-5">Back to Appointments</p>
+						onClick={() => navigate ("/appointments")}>
+						<ChevronLeftIcon className="w-6"/> <p className="text-lg px-5">Back
+						to Appointments</p>
 					</button>
-
+					
 					<div
 						className="flex justify-between items-center h-24 bg-ihs-green-shade-50 rounded-md shadow-sm text-gray-600">
 						<div className="flex">
 							<ClipboardCopyIcon className="md:w-14 w-8 md:ml-10 ml-3"/>
-							<h3 className="md:text-3xl text-2xl py-8 md:px-8 px-2">Book Appointment</h3>
+							<h3 className="md:text-3xl text-2xl py-8 md:px-8 px-2">Book
+								Appointment</h3>
 						</div>
 					</div>
-
+					
 					<form className="my-16 space-y-0" onSubmit={handleSubmit}>
-
+						
 						{/*Beneficiary*/}
 						<div className="flex md:flex-row flex-col">
-
+							
 							<div>
 								<label
 									htmlFor="beneficiary"
@@ -157,12 +191,12 @@ const BookAppointment = () => {
 										required
 										aria-required="true"
 										value={beneficiary}
-										onChange={(e) => setBeneficiary(e.target.value)}
+										onChange={(e) => setBeneficiary (e.target.value)}
 										className="w-full border border-gray-300 px-3 py-3 rounded-lg shadow-sm focus:outline-none focus:border:bg-ihs-green-shade-500 focus:ring-1 focus:ring-ihs-green-shade-600 md:w-72">
 										<option value="">Select Beneficiary</option>
 										{beneficiaries?.length
 											?
-											beneficiaries.map((beneficiary, index) => (
+											beneficiaries.map ((beneficiary, index) => (
 												<option
 													key={index}
 													value={beneficiary?.id}
@@ -177,10 +211,10 @@ const BookAppointment = () => {
 								</div>
 							</div>
 						</div>
-
+						
 						{/*Service*/}
 						<div className="flex md:pt-10 pt-5 md:flex-row flex-col">
-
+							
 							<div>
 								<label
 									htmlFor="service"
@@ -195,12 +229,12 @@ const BookAppointment = () => {
 										required
 										aria-required="true"
 										value={service}
-										onChange={(e) => setService(e.target.value)}
+										onChange={(e) => setService (e.target.value)}
 										className="w-full border border-gray-300 px-3 py-3 rounded-lg shadow-sm focus:outline-none focus:border:bg-ihs-green-shade-500 focus:ring-1 focus:ring-ihs-green-shade-600 text-gray-500 md:w-72">
 										<option value="">Select a service</option>
 										{services?.length
 											?
-											services.map((service, index) => (
+											services.map ((service, index) => (
 												<option
 													key={index}
 													value={service?.id}
@@ -214,11 +248,11 @@ const BookAppointment = () => {
 									</select>
 								</div>
 							</div>
-
+						
 						</div>
-
+						
 						<div className="flex md:pt-10 pt-5 md:flex-row flex-col">
-
+							
 							<div>
 								<label
 									htmlFor="date"
@@ -235,15 +269,15 @@ const BookAppointment = () => {
 										autoComplete="current-date"
 										aria-required="true"
 										value={date}
-										onChange={(e) => setDate(e.target.value)}
+										onChange={(e) => setDate (e.target.value)}
 										className="w-full border border-gray-300 px-3 py-3 rounded-lg shadow-sm focus:outline-none focus:border:bg-ihs-green-shade-500 focus:ring-1 focus:ring-ihs-green-shade-600 md:w-72"/>
 								</div>
 							</div>
-
+						
 						</div>
-
+						
 						<div className="flex md:pt-10 pt-5 md:flex-row flex-col">
-
+							
 							<div className="">
 								<label
 									htmlFor="time"
@@ -260,13 +294,13 @@ const BookAppointment = () => {
 										autoComplete="current-time"
 										aria-required="true"
 										value={time}
-										onChange={(e) => setTime(e.target.value)}
+										onChange={(e) => setTime (e.target.value)}
 										className="w-full border border-gray-300 px-3 py-3 rounded-lg shadow-sm focus:outline-none focus:border:bg-ihs-green-shade-500 focus:ring-1 focus:ring-ihs-green-shade-600 md:w-72"/>
 								</div>
 							</div>
-
+						
 						</div>
-
+						
 						<div className="flex justify-start">
 							<button type="submit"
 											className="px-4 py-3 my-20 bg-ihs-green hover:font-bold focus:outline-none focus:ring-2 focus:ring-ihs-green-shade-500 w-96 text-lg">
@@ -275,7 +309,8 @@ const BookAppointment = () => {
 						</div>
 					</form>
 				</div>
-				{toggleModal && <Modal setToggleModal={setToggleModal} executeFunction={redirectToPricingPage}
+				{toggleModal && <Modal setToggleModal={setToggleModal}
+															 executeFunction={redirectToPricingPage}
 															 message="Add a health Coverage to beneficiary?"
 															 header={"Beneficiary has no health coverage"}/>}
 			</>
