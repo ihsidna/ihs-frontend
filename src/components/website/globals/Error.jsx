52a9c50e562317b66d5851435480ed6a41cb3fcf
Anthony Chukwujekwu <nzubeanthony4@gmail.com>
2024-01-21 11:35:01 +0100

    fix change password UI redirection bug

--- src/components/website/globals/Error.jsx
+++ src/components/website/globals/Error.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Error404 from "../../../assets/images/error404.jpg";
 import Logo from "../../../assets/images/logo.svg";
 import { useNavigate } from "react-router-dom";
@@ -6,13 +6,17 @@ import { useNavigate } from "react-router-dom";
 const Error = () => {
   const navigate = useNavigate();
 
+  useEffect(() => {
+    navigate(-1);
+  }, [navigate]);
+
   return (
     <>
-      <div className="flex flex-col justify-center pt-24 flex justify-around items-center w-full h-full object-cover">
+      <div className="flex flex-col justify-center pt-24 items-center w-full h-full object-cover">
         <a href="https://ihsmia.com">
           <img src={Logo} className="w-44 lg:w-56" alt="ihs-logo" />
         </a>
-        <img src={Error404} alt="error404" />
+        <img src={Error404} alt="error404" className="w-1/2" />
         <button className="p-2 mt-10" onClick={() => navigate(-1)}>
           Go Back
         </button>
