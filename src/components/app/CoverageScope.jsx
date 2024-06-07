edabb7d6771457f27260b75fd8bd122aa9f74851
Anthony Chukwujekwu <nzubeanthony4@gmail.com>
2024-06-07 12:08:10 +0100

    update coverage limits

--- src/components/app/CoverageScope.jsx
+++ src/components/app/CoverageScope.jsx
@@ -4,167 +4,159 @@ import CheckSVG from "../shared/CheckSVG";
 const CoverageScope = () => {
   return (
     <>
-      <div className="sm:flex sm:items-center sm:justify-between mx-4">
-        <h2 className="md:text-3xl text-xl font-bold text-gray-800 mb-4">
+      <div className="mx-4 sm:flex sm:items-center sm:justify-between">
+        <h2 className="mb-4 text-xl font-bold text-gray-800 md:text-3xl">
           Coverage Scope
         </h2>
       </div>
-      <div className="grid gap-6 mt-16 sm:gap-8 sm:grid-cols-1 xl:grid-cols-2 mx-4 mb-20">
-        <div className="px-4 py-4 transition-colors duration-200 transform rounded-lg border shadow">
-          <p className="text-lg font-extrabold text-gray-800">0 - 69 Years</p>
+      <div className="grid gap-6 mx-4 mt-16 mb-20 sm:gap-8 sm:grid-cols-1 xl:grid-cols-2">
+        <div className="px-4 py-4 transition-colors duration-200 transform border rounded-lg shadow">
+          <p className="text-lg font-extrabold text-gray-800">0 - 65 Years</p>
           <small className="col-span-11 text-gray-500">
-            For individuals between the ages of 0 and 69
+            For individuals between the ages of 0 and 65
           </small>
-          <h4 className="mt-8 md:text-xl text-lg font-extrabold text-gray-800">
+          <h4 className="mt-8 text-lg font-extrabold text-gray-800 md:text-xl">
             Primary Services
           </h4>
           <div className="mt-4 space-y-4">
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
-              <span className="mx-4 col-span-11 text-gray-500">
+              <span className="col-span-11 mx-4 text-gray-500">
                 Routine Health Checks & Monitoring
               </span>
             </div>
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
+              <span className="col-span-11 mx-4 text-gray-500">
                 Medication Administration
               </span>
             </div>
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
+              <span className="col-span-11 mx-4 text-gray-500">
                 Post-Op & Ambulatory Care
               </span>
             </div>
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
+              <span className="col-span-11 mx-4 text-gray-500">
                 Secondary Care Resource, Coordination & Administration
               </span>
             </div>
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
+              <span className="col-span-11 mx-4 text-gray-500">
                 Health Coaching & Literacy Support
               </span>
             </div>
           </div>
 
-          <h4 className="mt-8 md:text-xl text-lg font-extrabold text-gray-800">
+          <h4 className="mt-8 text-lg font-extrabold text-gray-800 md:text-xl">
             Secondary Services & Limits
           </h4>
 
           <div className="mt-4 space-y-4">
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 text-gray-500 col-span-11">
+              <span className="col-span-11 mx-4 text-gray-500">
                 Inpatient – ₦ 2.5M/KSH 2M
               </span>
             </div>
 
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
-                Outpatient – ₦ 1.5M/KSH 150K
+              <span className="col-span-11 mx-4 text-gray-500">
+                Outpatient – ₦ 1M/KSH 150K
               </span>
             </div>
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
+              <span className="col-span-11 mx-4 text-gray-500">
                 Surgeries – ₦ 1M/KSH 400K
               </span>
             </div>
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
+              <span className="col-span-11 mx-4 text-gray-500">
                 Oncology Care – ₦ 1M/KSH 400K
               </span>
             </div>
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
-                Dialysis Care – 3 Sessions Annually
+              <span className="col-span-11 mx-4 text-gray-500">
+                Dialysis Care – 5 Sessions
               </span>
             </div>
 
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
-                Maternity & Neo-Natal – ₦ 300K/KSH 100K
+              <span className="col-span-11 mx-4 text-gray-500">
+                Maternity & Neo-Natal – ₦ 500K/KSH 100K
               </span>
             </div>
 
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
+              <span className="col-span-11 mx-4 text-gray-500">
                 Congenital Abnormalities (Pediatrics) – ₦ 250K/KSH 200K
               </span>
             </div>
 
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
-                Dental Care – ₦ 50K/KSH 15K
+              <span className="col-span-11 mx-4 text-gray-500">
+                Dental Care – ₦ 100K/KSH 15K
               </span>
             </div>
 
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
-                Optical Care – ₦ 75K/KSH 15K
+              <span className="col-span-11 mx-4 text-gray-500">
+                Optical Care – ₦ 50K/KSH 15K
               </span>
             </div>
 
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
+              <span className="col-span-11 mx-4 text-gray-500">
                 Medication Coverage (Chronic Diseases & Prescriptions)
               </span>
             </div>
 
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
-                Physiotherapy – ₦ 50K/KSH 100K
+              <span className="col-span-11 mx-4 text-gray-500">
+                Physiotherapy – ₦ 100K/KSH 100K
               </span>
             </div>
 
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
-                Travel Coverage (Global, 30 days)
+              <span className="col-span-11 mx-4 text-gray-500">
+                Travel Coverage (Global, 10 days)
               </span>
             </div>
 
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
-                Critical Illness & Death Coverage – ₦ 500K/KSH 400K
-              </span>
-            </div>
-
-            <div className="grid grid-cols-12 items-center">
-              <CheckSVG />
-
-              <span className="mx-4 col-span-11 text-gray-500">
+              <span className="col-span-11 mx-4 text-gray-500">
                 Death & Funeral Expenses – ₦ 100K/KSH 125K
               </span>
             </div>
@@ -173,127 +165,127 @@ const CoverageScope = () => {
           <br />
         </div>
 
-        <div className="px-6 py-4 transition-colors duration-200 transform rounded-lg border shadow">
-          <p className="text-lg font-extrabold text-gray-800">70 - 80 Years</p>
+        <div className="px-6 py-4 transition-colors duration-200 transform border rounded-lg shadow">
+          <p className="text-lg font-extrabold text-gray-800">65 - 85 Years</p>
           <small className="col-span-11 text-gray-500">
-            For individuals between the ages of 70 and 80
+            For individuals between the ages of 65 and 85
           </small>
 
-          <h4 className="mt-8 md:text-xl text-lg font-extrabold text-gray-800">
+          <h4 className="mt-8 text-lg font-extrabold text-gray-800 md:text-xl">
             Primary Services
           </h4>
 
           <div className="mt-4 space-y-4">
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
+              <span className="col-span-11 mx-4 text-gray-500">
                 Routine Health Checks & Monitoring
               </span>
             </div>
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
+              <span className="col-span-11 mx-4 text-gray-500">
                 Medication Administration
               </span>
             </div>
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
+              <span className="col-span-11 mx-4 text-gray-500">
                 Post-Op & Ambulatory Care
               </span>
             </div>
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
+              <span className="col-span-11 mx-4 text-gray-500">
                 Secondary Care Resource, Coordination & Administration
               </span>
             </div>
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
+              <span className="col-span-11 mx-4 text-gray-500">
                 Health Coaching & Literacy Support
               </span>
             </div>
           </div>
 
-          <h4 className="mt-8 md:text-xl text-lg font-extrabold text-gray-800">
+          <h4 className="mt-8 text-lg font-extrabold text-gray-800 md:text-xl">
             Secondary Services & Limits
           </h4>
 
           <div className="mt-4 space-y-4">
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 text-gray-500 col-span-11">
-                Inpatient – ₦ 1.5M/KSH 500K
+              <span className="col-span-11 mx-4 text-gray-500">
+                Inpatient – ₦ 1M/KSH 500K
               </span>
             </div>
 
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
-                Outpatient – ₦ 750K/KSH 350K
+              <span className="col-span-11 mx-4 text-gray-500">
+                Outpatient – ₦ 500K/KSH 350K
               </span>
             </div>
 
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
+              <span className="col-span-11 mx-4 text-gray-500">
                 Surgeries – ₦ 500K/KSH 250K
               </span>
             </div>
 
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
-                Oncology Care – ₦1.5M/KSH 500K
+              <span className="col-span-11 mx-4 text-gray-500">
+                Oncology Care – ₦1M/KSH 500K
               </span>
             </div>
 
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
+              <span className="col-span-11 mx-4 text-gray-500">
                 Dental Care – ₦ 50K/KSH 15K
               </span>
             </div>
 
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
-                Optical Care – ₦ 75K/KSH 15K
+              <span className="col-span-11 mx-4 text-gray-500">
+                Optical Care – ₦ 50K/KSH 15K
               </span>
             </div>
 
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
+              <span className="col-span-11 mx-4 text-gray-500">
                 Medication Coverage (Chronic Diseases & Prescriptions)
               </span>
             </div>
 
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
+              <span className="col-span-11 mx-4 text-gray-500">
                 Physiotherapy – ₦ 50K/KSH 50K
               </span>
             </div>
 
-            <div className="grid grid-cols-12 items-center">
+            <div className="grid items-center grid-cols-12">
               <CheckSVG />
 
-              <span className="mx-4 col-span-11 text-gray-500">
+              <span className="col-span-11 mx-4 text-gray-500">
                 Death & Funeral Expenses – ₦ 100K/KSH 100K
               </span>
             </div>
@@ -302,18 +294,18 @@ const CoverageScope = () => {
           <br />
 
           <div className="italic font-extralight">
-            <p className="lg:mx-4 text-gray-600">NB</p>
+            <p className="text-gray-600 lg:mx-4">NB</p>
 
-            <p className="lg:mx-4 text-gray-600 text-sm">
+            <p className="text-sm text-gray-600 lg:mx-4">
               - Chronic Disease Medications: Coverage Commences Month 2
             </p>
-            <p className="lg:mx-4 text-gray-600 text-sm">
+            <p className="text-sm text-gray-600 lg:mx-4">
               - Dental & Optical Care: Coverage Commences Month 3
             </p>
-            <p className="lg:mx-4 text-gray-600 text-sm">
+            <p className="text-sm text-gray-600 lg:mx-4">
               - Death & Funeral Expenses: Coverage Commences Month 6
             </p>
-            <p className="lg:mx-4 text-gray-600 text-sm">
+            <p className="text-sm text-gray-600 lg:mx-4">
               - Surgeries, Oncology & ICU Care: Coverage Commences Month 12
             </p>
           </div>
